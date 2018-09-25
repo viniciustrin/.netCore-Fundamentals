a214a583567c8c63d1d6bd9cfd1be61fb1d70a36
Vinicius Trindade <vinicius.trincid@gmail.com>
2018-09-25 17:57:18 -0300

    Commit inicial

---
+++ projetoteste/IGreeter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace projetoteste
+{
+    public interface IGreeter
+    {
+        string GetMessage();
+    }
+
+    public class Greeter : IGreeter
+    {
+        private IConfiguration _config;
+
+        public Greeter(IConfiguration config)
+        {
+            _config = config;        }
+        public string GetMessage()
+        {
+            return _config["Greeting"];
+        }
+    }
+}
---
+++ projetoteste/Startup.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace projetoteste
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IGreeter, Greeter>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app,
+                              IHostingEnvironment env,
+                              IGreeter greet,
+                              ILogger<Startup> logger)
+        {
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+
+            app.Use( next =>
+            {
+                return async context =>
+                {
+                    logger.LogInformation("Request incoming");
+                    if (context.Request.Path.StartsWithSegments("/mym"))
+                    {
+                        await context.Response.WriteAsync("Hit!!");
+                        logger.LogInformation("Request handled");
+                    }
+                    else
+                    {
+                        await next(context);
+                        logger.LogInformation("Request outgoing");
+                    }
+                };
+            });
+
+
+            app.Run(async (context) =>
+            {
+                var greeting = greet.GetMessage();
+                await context.Response.WriteAsync(greeting);
+            });
+        }
+    }
+}
